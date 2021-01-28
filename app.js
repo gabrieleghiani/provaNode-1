3c68dc5bc417bd6f032e4dd7f4c15ed3b48a36eb
Ghiani Gabriele <ghiani.gabriele@osdbsesto>
2021-01-28 13:11:34 +0100

    commit

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var http = require("http");
 var express = require("express");
+var apiServer = express();
 
 var hostname = "127.0.0.1";
 var port = 3000;
 
-var server = http.createServer((req, res) => {
-    console.log("req:", req);
-    res.statusCode = 200;
-    res.setHeader("content-Type", "text/plain");
-    res.end("Ciao belloccio!");
-});
-
 server.listen(port, hostname, () => {
     console.log("server running at http://%s:%d/", hostname, port);
 });
+
+apiServer.get("/nome", (req, res)=>{
+    console.log("ho ricevuto una get su nome");
+    res.send("ciao il nome del server è: node")
+});
+
+apiServer.get("/", function(req, res){
+    res.send("sei in home");
+})
